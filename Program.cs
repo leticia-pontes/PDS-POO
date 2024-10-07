92b6726098a6db78d93e3630c95acc79af052a28
Leticia Alves de Pontes <50830736+leticia-hub@users.noreply.github.com>
2024-10-07 20:49:32 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PDSPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
