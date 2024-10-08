e15006e7c050441266d252c8d5ccabf15197ef21
Leticia Alves de Pontes <50830736+leticia-hub@users.noreply.github.com>
2024-10-07 22:31:15 -0300

    Problema da Locadora - Parte 1

---
+++ Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDS_POO
+{
+    public class Cliente
+    {
+        public string Nome {  get; set; }
+        public List<Filme> Filmes { get; set; }
+
+        public Cliente(string nome)
+        {
+            Nome = nome;
+            Filmes = new List<Filme>();
+        }
+
+        public void AlugarFilme(Filme filme, Locadora locadora)
+        {
+            bool filmeExists = locadora.VerificarFilmeDisponivel(filme);
+
+            if (filmeExists) 
+            {
+                int unidadesDisponiveis = locadora.VerificarUnidadesFilme(filme);
+            }
+        }
+    }
+}
---
+++ Locadora.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDS_POO
+{
+    public class Locadora
+    {
+        public List<Cliente> Clientes {  get; set; }
+        public List<Filme> Filmes { get; set; }
+
+        public Locadora() {
+            Clientes = new List<Cliente>();
+            Filmes = new List<Filme>();
+        }
+
+        public bool VerificarFilmeDisponivel(Filme filme)
+        {
+            foreach (var f in Filmes)
+            {
+                if (f == filme)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public int VerificarUnidadesFilme(Filme filme)
+        {
+            return filme.Unidades;
+        }
+    }
+}
